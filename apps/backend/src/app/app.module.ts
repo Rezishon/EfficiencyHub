9095926c5f5e745cd165a7b5a4d0ae95bb2919c0
rezishon <rezashahmoradi.81@outlook.com>
2026-06-20 13:09:57 +0330

    feat[backend](app.module.ts): config to use redis

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,11 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import KeyvRedis from '@keyv/redis';
 import { TodoModule } from './todo/todo.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const host = configService.get<string>('REDIS_HOST');
+        const port = configService.get<number>('REDIS_PORT');
+        const password = configService.get<string>('REDIS_PASSWORD');
+
+        const redisUri = `redis://:${password}@${host}:${port}`;
+
+        return {
+          stores: [new KeyvRedis(redisUri)],
+          // ttl: 60000,
+        };
+      },
+    }),
+
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
