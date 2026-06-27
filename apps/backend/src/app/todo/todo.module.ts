19d79b79167747e158bb300ca993faff3dae48aa
rezishon <rezashahmoradi.81@outlook.com>
2026-06-27 11:50:15 +0330

    fix[backend](todo/todo.module.ts): fix prisma access from test files
    Inside the test files there was a problem which didn't init the prisma
    so I added it.

--- apps/backend/src/app/todo/todo.module.ts
+++ apps/backend/src/app/todo/todo.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
 import { PrismaService } from '../prisma.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [CacheModule.register()],
   controllers: [TodoController],
   providers: [TodoService, PrismaService],
+  exports: [TodoService, PrismaService],
 })
 export class TodoModule {}
