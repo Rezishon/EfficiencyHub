b6df4b1b2f945206f56888928f548e406490fee0
rezishon <rezashahmoradi.81@outlook.com>
2026-07-02 10:57:48 +0330

    feat[frontend](app/app.routes.ts): add `todo` route
    The todo page will handle todo feature of our website.

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page';
+import { TodoPage } from './pages/todo-page/todo-page';
 
 export const routes: Routes = [
   {
     path: '',
     component: LandingPage,
+  },
+  {
+    path: 'todo',
+    component: TodoPage,
   },
 ];
