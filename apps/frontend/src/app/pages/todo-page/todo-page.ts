c7c07f7e0bdfa298993548caae185bb950e98301
rezishon <rezashahmoradi.81@outlook.com>
2026-07-02 11:18:06 +0330

    feat[frontend](pages/todo-page): add `TodoPage` component

---
+++ apps/frontend/src/app/pages/todo-page/todo-page.ts
@@ -0,0 +1,57 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodoService } from '../../services/todo-service/todo-service';
+import { IApiTodoList, ITodoItem } from '@riseof-website/shared-types';
+import { TodoItem } from '../../components/todo-item/todo-item';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Dialog } from '../../components/dialog/dialog';
+import { Toast } from '../../components/toast/Toast';
+
+@Component({
+  selector: 'app-todo-page',
+  imports: [TodoItem, CdkDropList, Dialog, Toast],
+  templateUrl: './todo-page.html',
+  styleUrl: './todo-page.css',
+})
+export class TodoPage implements OnInit {
+  private todoService = inject(TodoService);
+  private currentListName = signal<string>('');
+  todoLists = signal<IApiTodoList[]>([]);
+  todoItems = signal<ITodoItem[]>([]);
+
+  ngOnInit(): void {
+    this.todoService.getTodoLists().subscribe((res) => {
+      this.todoLists.set(res.data);
+
+      if (res.data && res.data.length > 0) {
+        this.getItemsOfList(res.data[0].name);
+      }
+    });
+  }
+
+  getItemsOfList(listName: string) {
+    this.todoService.getTodoList(listName).subscribe((res) => {
+      this.todoItems.set(res.data.items);
+    });
+
+    this.currentListName.set(listName);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.todoItems(), event.previousIndex, event.currentIndex);
+  }
+
+  onDataUpdate(item: ITodoItem | undefined) {
+    if (item) {
+      this.todoService.patchTodoItem(this.currentListName(), item).subscribe((res) => {
+        this.todoItems.set(res.data);
+      });
+    }
+  }
+
+  addNewTodoList(listName: string) {
+    this.todoService.addTodoList(listName).subscribe((res) => {
+      this.ngOnInit();
+      console.log(res);
+    });
+  }
+}
