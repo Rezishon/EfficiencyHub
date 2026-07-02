cf4f32f2cf5d07141bb69b8d251e1772e8963edf
rezishon <rezashahmoradi.81@outlook.com>
2026-07-02 11:03:05 +0330

    feat[frontend](components/dialog): add `dialog` component
    The dialog build for one perpose for now. When we need more features or
    more generic component I will change it then.

---
+++ apps/frontend/src/app/components/dialog/dialog.ts
@@ -0,0 +1,56 @@
+import { Component, ElementRef, inject, input, output, signal, viewChild } from '@angular/core';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { LucideCircleX } from '@lucide/angular';
+import { timer } from 'rxjs';
+import { ToastService } from '../../services/toast/toast-service';
+import { Toast } from '../toast/Toast';
+
+@Component({
+  selector: 'app-dialog',
+  imports: [LucideCircleX, ReactiveFormsModule, Toast],
+  templateUrl: './dialog.html',
+  styleUrl: './dialog.css',
+})
+export class Dialog {
+  toastService = inject(ToastService);
+
+  id = input.required<string>();
+  label = input.required<string>();
+  optionalLabel = input<string>('');
+  placeholder = input<string>('');
+  inputValue = output<string>();
+
+  dialogRef = viewChild<ElementRef<HTMLDialogElement>>('dialogRef');
+
+  formControl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(1)],
+  });
+
+  openModal() {
+    this.dialogRef()?.nativeElement.showModal();
+  }
+
+  closeModal() {
+    this.dialogRef()?.nativeElement.close();
+
+    timer(1000).subscribe(() => {
+      this.formControl.reset('');
+      this.formControl.markAsUntouched();
+      this.formControl.markAsPristine();
+    });
+  }
+
+  onSave() {
+    if (this.formControl.valid) {
+      this.inputValue.emit(this.formControl.value);
+
+      this.closeModal();
+    } else {
+      this.closeModal();
+      timer(100).subscribe(() => {
+        this.toastService.toasterOn('List name should not be empty');
+      });
+    }
+  }
+}
